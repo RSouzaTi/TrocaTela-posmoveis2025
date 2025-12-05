049df0a1d7e572b24a866c5dc1ce58764b13be6c
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2025-12-05 12:24:40 -0300

    Aplicativo com a tela de confirmarActivity pronta, com a codificaçãodo botão confirmar da tela de lançamento, e recebendo dados desta tela na ConfirmarActivity

---
+++ app/src/main/java/br/edu/utfpr/trocatela_posmoveis2025/ConfirmarActivity.kt
@@ -0,0 +1,40 @@
+package br.edu.utfpr.trocatela_posmoveis2025
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class ConfirmarActivity : AppCompatActivity() {
+
+    private lateinit var tvCod: TextView
+    private lateinit var tvQtd: TextView
+    private lateinit var tvValor: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_confirmar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        tvCod = findViewById(R.id.tvCod)
+        tvQtd = findViewById(R.id.tvQtd)
+        tvValor = findViewById(R.id.tvValor)
+
+        //codigo recuperado como inteiro, pois foi transferido como inteiro na tela de lançamento
+        val cod: Int = intent.getIntExtra("cod", 0)
+        val qtd = intent.getStringExtra("qtd")
+        val valor = intent.getStringExtra("valor")
+
+        tvCod.text = cod.toString()
+        tvQtd.text = qtd
+        tvValor.text = valor
+
+    } //fim do onCreate
+}
